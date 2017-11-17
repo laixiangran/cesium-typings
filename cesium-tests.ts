76cde4d4198cce2a632cee47cc1dacb71af8dcdf
laixiangran <laixiangran@163.com>
2017-11-17 11:52:12 +0800

    添加截图

--- cesium-tests.ts
+++ cesium-tests.ts
@@ -6,7 +6,7 @@
 /// <reference path="./index.d.ts" />
 
 const view = new Cesium.Viewer('');
-Cesium.BingMapsApi.defaultKey = 'AihaXS6TtE_olKOVdtkMenAMq1L5nDlnU69mRtNisz1vZavr1HhdqGRNkB2Bcqvs'; // For use with this application only
+Cesium.BingMapsApi.defaultKey = 'AihaXS6TtE_olKOVdtkMenAMq1L5nDlnU69mRtNisz1vZavr1HhdqGRNkB2Bcqvs';
 // Add Bing imagery
 view.imageryLayers.addImageryProvider(new Cesium.BingMapsImageryProvider({
     url: 'https://dev.virtualearth.net',
